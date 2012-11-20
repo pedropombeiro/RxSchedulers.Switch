97efe3fb1f6bcd4de02b0c58080d5a03b5142c47
Pedro Pombeiro <pedropombeiro@gmail.com>
2012-11-20 13:30:25 +0100

    Initial commit.

---
+++ Update-Repo.ps1
@@ -0,0 +1,8 @@
+﻿while ($true)
+{
+	git.exe fetch --all
+	
+	Write-Host "Waiting 30 seconds..."
+	
+	sleep 30
+}
---
+++ solution/src/app/RxSchedulers.Switch/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Pedro Pombeiro">
+//   © 2012 Pedro Pombeiro. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RxSchedulers.Switch")]
+[assembly: AssemblyDescription("")]
